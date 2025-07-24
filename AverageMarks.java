09002ecaf02d58b085850d786fd7feb7ac7c9d13
keshav5467 <keshavgoel166@gmail.com>
2025-07-24 22:28:20 +0530

    Add AverageMarks.java to calculate PCM average

---
+++ AverageMarks.java
@@ -0,0 +1,10 @@
+class Average {
+    public static void main(String[] args) {
+       int maths=94;
+       int physics=95;
+       int chemistry=96;
+       int total=maths+physics+chemistry;
+       double average=total/3.0;
+       System.out.println(average);
+    }
+}
