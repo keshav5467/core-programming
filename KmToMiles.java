6e8d11b6f31a779d47b46991f0a557d14f4a162c
keshav5467 <keshavgoel166@gmail.com>
2025-07-24 22:55:21 +0530

    distance coversion from km to miles

---
+++ KmToMiles.java
@@ -0,0 +1,7 @@
+class Distance{
+    public static void main(String[] args) {
+       double distanceinkm =10.8;
+       double distanceinmiles=10.8*1.6;
+       System.out.println(distanceinmiles);
+    }
+}
